53950d50ddbbdbab18f7b8f4d03039c1b74887c5
Perri <perriluo05@gmail.com>
2026-07-24 16:43:31 +0800

    Restore: Recover emptied component files

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH || '',
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
